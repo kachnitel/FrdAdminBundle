cb36756e219d7b6677aef619f4bcf287eed519c6
Ondrej Vana <vana.ondrej@gmail.com>
2025-11-27 22:47:22 -0800

    fix: Load services and Twig paths in bundle configuration

--- src/FrdAdminBundle.php
+++ src/FrdAdminBundle.php
@@ -2,6 +2,8 @@
 
 namespace Frd\AdminBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class FrdAdminBundle extends AbstractBundle
@@ -9,5 +11,21 @@ class FrdAdminBundle extends AbstractBundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // Load services
+        $container->import('../config/services.yaml');
+    }
+
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // Register Twig namespace
+        $container->extension('twig', [
+            'paths' => [
+                $this->getPath() . '/templates' => 'FrdAdmin',
+            ],
+        ]);
     }
 }
